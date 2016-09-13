6877caea561b3eca5294bad69ca02df1209a7056
sc89703312 <89703312@qq.com>
2016-09-13 13:17:31 +0800

    bu
    emt.

--- src/main/java/nju/citicup/data/impl/OptionStateServiceImpl.java
+++ src/main/java/nju/citicup/data/impl/OptionStateServiceImpl.java
@@ -1,9 +1,11 @@
 package nju.citicup.data.impl;
 
+import nju.citicup.common.dataservice.OptionStateDataService;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by lenovo on 2016/9/13.
  */
 @Component
+public class OptionStateServiceImpl implements OptionStateDataService {
 }
