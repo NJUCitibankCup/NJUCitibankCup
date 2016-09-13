6f502eb6f508d3a5fd69f4df9eff80045335e3ab
PolarisChen <polaris_chen@163.com>
2016-09-14 00:06:57 +0800

    Add functions to get futures

--- src/main/resources/static/sell/sell.js
+++ src/main/resources/static/sell/sell.js
@@ -1,15 +1,91 @@
 /**
- * Created by Alan on 2016/9/7.
+ * Created by PolarisChen on 2016/9/14.
  */
 
-/**
- * 监听是否选择障碍期权
- * @param select
- */
+$(function() {
+  getFuturesType();
+  var currentType = $('#futures_type').val();
+  getFuturesId(currentType);
+})
+
+$('#futures_type').change(function() {
+  var type = $(this).val();
+  getFuturesId(type);
+})
+
+function getFuturesType() {
+  var $type = $('#futures_type');
+
+  // var url = '/api/getFuturesType';
+  // $.ajax({
+  //   type: 'GET',
+  //   url: url,
+  //   dataType: 'json',
+  //   success: function(res) {
+  //     $type.html('');
+  //     res.data.map(function(type) {
+  //       $type.append('<option>' + type + '</option>');
+  //     });
+  //   }
+  // });
+
+  var res = {
+  	msg: '',
+    condition: 'success',
+    data: [
+    	"玉米", "大豆", "豆粕"
+    ]
+  }
+  $type.html('');
+  res.data.map(function(type) {
+    $type.append('<option>' + type + '</option>');
+  });
+}
+
+function getFuturesId(type) {
+  var $id = $('#futures_id');
+  // var url = '/api/getFuturesId';
+  // var data = {
+  //   type: type
+  // }
+  // $.ajax({
+  //   type: 'GET',
+  //   url: url,
+  //   data: data,
+  //   dataType: 'json',
+  //   success: function(res) {
+  //     $id.html('');
+  //     res.data.map(function(id) {
+  //       $type.append('<option>' + id.futures_name + '</option>');
+  //     });
+  //   }
+  // });
+
+  var res = {
+  	msg: "",
+    condition: "success",
+    data: [
+      {
+        futures_id: "M1701",
+        futures_name: type + "1701"
+      },
+      {
+        futures_id: "M1702",
+        futures_name: type + "1702"
+      },
+    ]
+  }
+
+  $id.html('');
+  res.data.map(function(id) {
+    $id.append('<option>' + id.futures_name + '</option>');
+  });
+}
+
 function changeOptionType(select) {
-    if ($(select).val() == "障碍期权") {
-        $('#block-level').show(300);
-    } else {
-        $('#block-level').hide(300);
-    }
+ if ($(select).val() === "障碍期权") {
+   $('#block-level').show(300);
+ } else {
+   $('#block-level').hide(300);
+ }
 }
